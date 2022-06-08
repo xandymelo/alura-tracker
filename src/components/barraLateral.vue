3e6f527276c795b28ed72c5e30e2ad4f5baec09e
Alexandre Melo <xandymelo@outlook.com.br>
2022-06-08 02:40:11 -0300

    criação da barraLateral e do Formulário

---
+++ src/components/barraLateral.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="alura-tracker">
+        </h1>
+    </header>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        name: 'BarraLateral'
+    })
+</script>
+
+<style>
+    header {
+    padding: 1rem;
+    background: #FFD700;
+    width: 100%;
+    height: 100vh;
+    border-bottom-right-radius: 10px;
+    border-top-right-radius: 10px;
+    }
+    @media only screen and (max-width: 768px) {
+        header {
+            padding: 2.5rem;
+            height: auto;
+        }
+    }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@fortawesome/fontawesome-free/css/all.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+// Optionally install the BootstrapVue icon components plugin
 createApp(App).mount('#app')
